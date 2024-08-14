9d3f40840343b30808dccf9b50f3bbadb79c0d2b
Matt Wilkerson <mattwilkerson1121@gmail.com>
2024-08-14 09:28:12 -0400

    Update swatch-table.sql
    Boolean default values changed to be 1 or 0 and updated eco_order to type of decimal for more precise ordering values.

--- src/swatch-table.sql
+++ src/swatch-table.sql
@@ -6,8 +6,8 @@ CREATE TABLE details (
  color VARCHAR(128),
  fabric VARCHAR(128),
  description TEXT NOT NULL,
- is_eco BOOLEAN DEFAULT FALSE,
- eco_order INT DEFAULT 0,
+ is_eco BOOLEAN DEFAULT 0,
+ eco_order DECIMAL DEFAULT 0.0,
  attributes VARCHAR(1024),
  cleancode VARCHAR(2),
  is_available BOOLEAN DEFAULT 1
